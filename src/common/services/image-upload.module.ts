6591807d8e7b2cf6e41e8d50abafbb064a8234bf
HarudesDevRv <luisrendon@ravn.co>
2026-02-12 15:48:05 -0500

    Fix: Fixed Image Upload Module providers

--- src/common/services/image-upload.module.ts
+++ src/common/services/image-upload.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ImageUploadService } from 'src/common/services/image-upload.service';
 
 @Module({
+  providers: [ImageUploadService],
   exports: [ImageUploadService],
 })
 export class ImageUploadModule {}
